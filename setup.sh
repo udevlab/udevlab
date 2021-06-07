56d7702558f749cacb1ac2d73bedf659157cc3d6
João Pinto <lamego.pinto@gmail.com>
2021-06-07 22:14:16 +0200

    Fixed http alies

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -eu # Fail on error and undefined variables
+
+function check_code() {
+    CODE_VERSION=$(code --version)
+    echo "code:" $CODE_VERSION
+}
+
+function check_conda {
+    CONDA_VERSION=$(~/miniforge3/condabin/conda.bat --version)
+    echo $CONDA_VERSION
+}
+
+function check_requirements() {
+    check_code
+    check_conda
+}
+
+function get_setup_py() {
+    SETUP_TMP_DIR=$(mktemp -d)
+    trap 'rm -rf "$SETUP_TMP_DIR"' EXIT
+    curl -sf https://udevlab.org/setup.py -o $SETUP_TMP_DIR/setup.py || curl -f https://udevlab.org/setup.py
+}
+
+check_requirements
+get_setup_py
+
+# Activate the base miniforge environment
+~/miniforge3/condabin/conda.bat init bash > /dev/null
+echo Creating the udevlab conda environment...
+~/miniforge3/condabin/conda.bat create -y --name udevlab python=3 > /dev/null
+~/miniforge3/python $SETUP_TMP_DIR/setup.py
+
+# Set aliases to use winpty on gitbash
+grep "alias python" ~/.bash_profile >/dev/null || echo "alias python='winpty python'" >> ~/.bash_profile
+grep "alias ptpython" ~/.bash_profile >/dev/null || echo "alias ptpython='winpty ptpython'" >> ~/.bash_profile
+grep "alias http" ~/.bash_profile >/dev/null || echo "alias http='winpty http'" >> ~/.bash_profile
+grep "conda activate udevlab" ~/.bash_profile >/dev/null || echo "conda activate udevlab" >> ~/.bash_profile
+
+git config --global core.autocrlf input
+
+echo "---"
+echo "Restart your GitBash session for the changes to be effective"
